9cc31d4b734d85232311182e087062ad9356e3fa
Mahdi AlKhalaf <m@mahdi.pro>
2020-07-20 23:00:57 +0300

    Add initial implementation of the homepage

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return \
+        """
+        <form method='post' action='http://localhost:5000/hello'>
+            <input type='text' name='name' placeholder='Name'>
+            <input type='submit'>
+        </form>
+        """
 
 
 @app.route("/hello")
 def hello():
     return "Hello"
+
+
+@app.route("/hello", methods=['POST'])
+def hello_name():
+    name = request.form['name']
+    return f"Hello {name}"
 
 
 if __name__ == '__main__':
